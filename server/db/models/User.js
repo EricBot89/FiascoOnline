0c70b0ece6a62b67d1c243e8330b592b699189ae
EricBot89 <eloucks@gmail.com>
2019-08-24 10:44:11 -0400

    completes user model

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,6 +1,59 @@
 const Sequelize = require("sequelize");
 const db = require("../index").db;
+const crypto = require("crypto");
 
-module.exports = db.define('user', {
-    userName: Sequelize.STRING
-})
+module.exports = db.define("user", {
+  userName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    get() {
+      return () => this.getDataValue("password");
+    }
+  },
+  salt: {
+    type: Sequelize.STRING,
+    get() {
+      return () => this.getDataValue("salt");
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    uniqe: true,
+    allowNull: false,
+    vaidate: {
+      isEmail: true
+    }
+  }
+});
+
+User.prototype.validatePW = incomingPW => {
+  return User.encryptPW(incomingPW, this.salt()) === this.password();
+};
+
+User.generateSalt = () => {
+  return crypto.randomBytes(16).toString("hex");
+};
+
+User.encryptPW = (pw, salt) => {
+  return crypto
+    .createHmac("SHA256", pw)
+    .update(salt)
+    .digest("hex");
+};
+
+const encryptCredentials = user => {
+  user.salt = User.generateSalt();
+  user.password = User.encryptPW(user.password(), user.salt());
+};
+
+User.beforeCreate(encryptCredentials);
+User.beforeUpdate(encryptCredentials);
+User.beforeBulkCreate(userList => {
+  userList.forEach( user => {
+    encryptCredentials(user);
+  });
+});
