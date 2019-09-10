bcd67c35f06425691610b1d09ddccb18331ddfa9
EricBot89 <eloucks@gmail.com>
2019-09-10 07:53:26 -0400

    adds buttons for room list interaction

--- client/comps/Rooms.js
+++ client/comps/Rooms.js
@@ -1,33 +1,33 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-import {connect} from "react-redux"
+import { connect } from "react-redux"
 
 
 const DCRooms = (props) => {
-  const {rooms} = props
+  const { rooms } = props
   return (
     <div className="rooms-container">
       <div className="roomsTools">
         <button>Create Game</button>
         <button>Join Game With Code</button>
         <button>Refresh Game List</button>
- 
-        </div>
-    <div className="room-list" >
-      {rooms.map( (room, idx) => (
-        <div className='room-card' key={idx}>
-          <Link to={`/game/${room.id}`}>{room.name}</Link>
-        </div>
-      ))}
-    </div >
+
+      </div>
+      <div className="room-list" >
+        {rooms.map((room, idx) => (
+          <div className='room-card' key={idx}>
+            <Link to={`/game/${room.id}`}>{room.name}</Link>
+          </div>
+        ))}
+      </div >
     </div>
   )
 }
 
 const mapState = state => ({
-    rooms: state.gameList.openGames
+  rooms: state.gameList.openGames
 })
 
-const Rooms = connect(mapState,null)(DCRooms)
+const Rooms = connect(mapState, null)(DCRooms)
 
 export { Rooms }
