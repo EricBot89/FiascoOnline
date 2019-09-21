bca87177734a86e92ad6f345496c87b10f704cb8
EricBot89 <eloucks@gmail.com>
2019-09-21 18:41:58 -0400

    sets up db protocalls for saving chat logs

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,7 +1,16 @@
 const User = require("./User");
 const Chat = require("./Chat");
+const Game = require("./Game")
+
+User.hasMany(Game)
+
+Game.hasMany(Chat)
+Chat.belongsTo(Game)
+
+Chat.belongsTo(User)
 
 module.exports = {
   User,
-  Chat
+  Chat,
+  Game
 };
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,6 +1,15 @@
 const { ChacheLib } = require("./chatCache");
+const { User, Game, Chat} = require("../db/models")
 
 const cachedChats = new ChacheLib();
+
+const  storeChat = async (mssg, locale) => {
+  try{
+    Chat.create({ messageText: mssg, locale})
+  } catch (err){
+    console.log(err)
+  }
+}
 
 module.exports = io => {
   io.on("connection", socket => {
@@ -21,18 +30,19 @@ module.exports = io => {
       }
     });
 
-    socket.on("chatMessage", (user, mssg, locale) => {
+    socket.on("chatMessage", (user, mssg, locale, userID) => {
       const currentTime = new Date();
       const mssgString = `[${user}: ${currentTime.getHours()}:${currentTime.getMinutes()}] ${mssg}`;
       const cache = cachedChats.get(locale);
       cache.addChat(mssgString);
       if (locale !== "Global") {
         //save this to the db
+        storeChat(mssg, locale)
       }
       io.sockets.in(locale).emit("newChatMessage", mssgString);
     });
 
-    socket.on("createRoom", room => {
+    socket.on("createRoom", (room, userID) => {
       cachedChats.set(room);
       //add the new game to the db
       socket.emit.broadcast("roomCreated", room);
